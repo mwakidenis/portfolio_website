3bff1410f0e318a107b59a4f798ea91f47ef17bb
MERN Stack Developer <mwakidenice@gmail.com>
2026-02-04 13:16:52 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,17 @@ export default {
 	prefix: "",
 	theme: {
 		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px'
-			}
+  center: true,
+  padding: '1rem', // smaller padding for mobile
+  screens: {
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    '2xl': '1400px',
+  }
+}
+
 		},
 		extend: {
 			fontFamily: {
